f438b7a5ba50d625abbc2b5d78e65d351e27a7b0
Andro <andrey.v.georgiev@gmail.com>
2018-01-16 13:27:06 +0200

    Added the first EX from JS Advanced

--- _01_JavascriptFundamentals/_01_javascriptSyntaxLAB/_07_nextDay.js
+++ _01_JavascriptFundamentals/_01_javascriptSyntaxLAB/_07_nextDay.js
@@ -4,5 +4,4 @@ function calcNextDay(year, month, day) {
     let oneDay = 24 * 60 * 60 * 1000;
     let nextDate = new Date(date.getTime() + oneDay);
     return nextDate.getFullYear() + "-" + (nextDate.getMonth() + 1) + "-" + nextDate.getDate();
-
 }
