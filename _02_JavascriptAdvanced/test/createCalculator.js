4e025cb81f857cc45e95ceabe6c1851ef9c4b265
Andro <andrey.v.georgiev@gmail.com>
2018-02-02 15:38:33 +0200

    Added P6 / Unit Testing LAB

---
+++ _02_JavascriptAdvanced/test/createCalculator.js
@@ -0,0 +1,3 @@
+let createCalculator = require('../_06_unitTesting/_07_addSubtract/createCalculator.js').createCalculator;
+let expect = require('chai').expect;
+
---
+++ _02_JavascriptAdvanced/test/rgbToHex.js
@@ -0,0 +1,29 @@
+let rgbToHexColor = require('../_06_unitTesting/_06_rgbToHex/rgbToHex.js').rgbToHexColor;
+let expect = require("chai").expect;
+
+describe("RGB To Hex Color", () => {
+    it("should return #FF9EAA for (255, 158, 170)", () => {
+        expect(rgbToHexColor(255, 158, 170)).to.be.equal('#FF9EAA')
+    });
+    it("should pad values with zeroes", () => {
+        expect(rgbToHexColor(12, 13, 14)).to.be.equal('#0C0D0E')
+    });
+    it("should work at lower limit", () => {
+        expect(rgbToHexColor(0, 0, 0)).to.be.equal('#000000')
+    });
+    it("should work at upper limit", () => {
+        expect(rgbToHexColor(255, 255, 255)).to.be.equal('#FFFFFF')
+    });
+    it("should return undefined for greater than 255 values", () => {
+        expect(rgbToHexColor(26, 25, 256)).to.be.equals(undefined);
+    });
+    it("should return undefined for negative values", () => {
+        expect(rgbToHexColor(-1, -5, -18)).to.be.equals(undefined);
+    });
+    it("should return undefined for floating point values", () => {
+        expect(rgbToHexColor(2.6, 3.14, 10.2)).to.be.equals(undefined);
+    });
+    it("should return undefined for invalid values", () => {
+        expect(rgbToHexColor("Pesho",{name: "Gosho"}, ['Ivan'])).to.be.equals(undefined);
+    });
+});
