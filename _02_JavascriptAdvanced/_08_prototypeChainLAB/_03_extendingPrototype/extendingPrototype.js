e7bf0d1222edd4694938c7ad5ca9a92228eb3169
Andro <andrey.v.georgiev@gmail.com>
2018-02-03 14:07:52 +0200

    Added P3, P4 / Prototype Chain LAB

---
+++ _02_JavascriptAdvanced/_08_prototypeChainLAB/_03_extendingPrototype/extendingPrototype.js
@@ -0,0 +1,41 @@
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    toString() {
+        let className = this.constructor.name;
+        return `${className} (name: ${this.name}, email: ${this.email})`;
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, email, subject) {
+        super(name, email);
+        this.subject = subject;
+    }
+
+    toString() {
+        let baseString = super.toString().slice(0, -1);
+        return baseString + `, subject: ${this.subject})`;
+    }
+}
+
+class Student extends Person {
+    constructor(name, email, course) {
+        super(name, email);
+        this.course = course;
+    }
+}
+
+function extendPrototype(baseClass) {
+    baseClass.prototype.species = 'Human';
+    baseClass.prototype.toSpeciesString = function () {
+        return  `I am a ${this.species}. ${this.toString()}`
+    }
+}
+
+
+
+
