fc8877da77acb81040520d2e92490c61227b90db
Andro <andrey.v.georgiev@gmail.com>
2018-02-14 20:35:24 +0200

    Added p3 / DOM manipulation EX

---
+++ _02_JavascriptAdvanced/_03_domManipulationEX/_03_accordion/accordeon.js
@@ -0,0 +1,10 @@
+function toggle() {
+    let spanFirst = document.getElementsByClassName('button')[0];
+    if( spanFirst.textContent === 'More'){
+        document.getElementsByClassName('button')[0].textContent = 'Less';
+        document.getElementById("extra").style.display = "block";
+    }else{
+        document.getElementsByClassName('button')[0].textContent = 'More';
+        document.getElementById("extra").style.display = "none";
+    }
+}
