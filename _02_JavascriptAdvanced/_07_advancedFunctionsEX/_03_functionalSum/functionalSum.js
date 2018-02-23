1ae52ec2ed4dbeae6a350fd4f46d067a5e9336e1
Andro <andrey.v.georgiev@gmail.com>
2018-02-23 12:57:52 +0200

    Added P3 // Advanced functions EX

---
+++ _02_JavascriptAdvanced/_07_advancedFunctionsEX/_03_functionalSum/functionalSum.js
@@ -0,0 +1,14 @@
+function add(n) {
+    sum = n;
+    const proxy = new Proxy(function a () {}, {
+        get (obj, key) {
+            return () => sum;
+        },
+        apply (receiver, ...args) {
+            sum += args[1][0];
+            return proxy;
+        },
+    });
+    return proxy
+}
+console.log(add(1)(2)(3)(10));
