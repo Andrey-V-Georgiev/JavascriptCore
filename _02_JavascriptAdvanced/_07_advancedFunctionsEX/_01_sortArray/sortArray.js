80e503af01ab9d012d495df14707ea3b7dc97903
Andro <andrey.v.georgiev@gmail.com>
2018-02-23 09:10:24 +0200

    Added P1 // Advanced functions EX

---
+++ _02_JavascriptAdvanced/_07_advancedFunctionsEX/_01_sortArray/sortArray.js
@@ -0,0 +1,16 @@
+function solve(arr, direction){
+    let ascendingComparator = function (a, b){
+        return a - b;
+    };
+    let descendingComparator = function (a, b){
+        return b - a;
+    };
+    let sortingStrategies = {
+        'asc': ascendingComparator,
+        'desc': descendingComparator
+    };
+
+    return arr.sort(sortingStrategies[direction]);
+}
+
+console.log(solve([14, 7, 17, 6, 8], 'asc'));
