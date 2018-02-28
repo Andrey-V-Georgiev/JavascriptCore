2e288e85db8ec812b5f6c6fc76d8ce7ebf5520d9
Andro <andrey.v.georgiev@gmail.com>
2018-02-28 22:52:06 +0200

    Added P3 // Unit Testing EX

--- _02_JavascriptAdvanced/test/_11_unitTestingEX.test/_02_evenOrOdd.js
+++ _02_JavascriptAdvanced/test/_11_unitTestingEX.test/_02_evenOrOdd.js
@@ -1,20 +1,20 @@
 let isOddOrEven = require('../../_11_unitTestingEX/_02_evenOrOdd/evenOrOdd.js').isOddOrEven;
-let expect = require("chai").expect;
+let chai = require("chai");
 
 describe("_02_evenOrOdd", function () {
     it("Testing is it String (by number)", function () {
-        expect(isOddOrEven(9)).to.be.equal(undefined, 'Wrong output');
+        chai.expect(isOddOrEven(9)).to.be.equal(undefined, 'Wrong output');
     });
 
     it("Testing is it String (by object)", function () {
-        expect(isOddOrEven({obj: 8})).to.be.equal(undefined, 'Wrong output');
+        chai.expect(isOddOrEven({obj: 8})).to.be.equal(undefined, 'Wrong output');
     });
 
     it("Testing is it Odd", function () {
-        expect(isOddOrEven('odd')).to.be.equal('odd','The string length is not odd number');
+        chai.assert.equal(isOddOrEven('odd'),'odd','The string length is not odd number');
     });
 
     it("Testing is it Even", function () {
-        expect(isOddOrEven('even')).to.be.equal('even','The string length is not even number');
+        chai.expect(isOddOrEven('even')).to.be.equal('even','The string length is not even number');
     });
 });
---
+++ _02_JavascriptAdvanced/test/_11_unitTestingEX.test/_03_charLookup.js
@@ -0,0 +1,33 @@
+let lookupChar = require('../../_11_unitTestingEX/_03_charLookup/charLookup.js').lookupChar;
+let chai = require('chai');
+
+describe('_03_charLookup',()=> {
+    it('test by number and number', ()=> {
+        chai.assert.equal(lookupChar(3, 3), undefined, 'Function should return undefined');
+    });
+
+    it('test by string and string', ()=> {
+        chai.assert.equal(lookupChar('str', 'str'), undefined, 'Function should return undefined');
+    });
+    it('test by string and float', () => {
+        chai.assert.equal(lookupChar('str', 3.14), undefined, 'Function should return undefined');
+    });
+    it('testing by incorrect index', () => {
+        chai.expect(lookupChar('str', 7)).to.equal('Incorrect index', 'Index out of bound');
+    });
+
+    it('testing by negative index', ()=> {
+        chai.expect(lookupChar('str', -3)).to.equal('Incorrect index', 'Index out of bound');
+    });
+
+    it('testing by index equals to string length', () => {
+        chai.expect(lookupChar('str', 'str'.length)).to.equal('Incorrect index', 'Index out of bound');
+    });
+    it('testing by zero index', ()=> {
+        chai.assert.equal(lookupChar('str', 0), 's', 'Wrong return value');
+    });
+
+    it('testing by correct value', ()=> {
+        chai.assert.equal(lookupChar('str', 2), 'r', 'Wrong return value');
+    });
+});
