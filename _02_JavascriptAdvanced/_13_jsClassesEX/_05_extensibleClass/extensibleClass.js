a7b7658548371edcf6af3e1258b7602857e05ef8
Andro <andrey.v.georgiev@gmail.com>
2018-03-06 23:36:59 +0200

    Added P5 // JS Classes EX

---
+++ _02_JavascriptAdvanced/_13_jsClassesEX/_05_extensibleClass/extensibleClass.js
@@ -0,0 +1,20 @@
+(function () {
+    let id = 0;
+    return class Extensible {
+        constructor() {
+            this.id = id++
+        }
+        extend(template) {
+            for (let prop in template) {
+                if (typeof template[prop] === 'function') {
+                    Object.getPrototypeOf(this)[prop] = template[prop]
+                } else {
+                    this[prop] = template[prop]
+                }
+            }
+        }
+    }
+}());
+
+
+
