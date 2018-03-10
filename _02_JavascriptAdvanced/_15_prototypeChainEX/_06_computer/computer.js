2a09e5fbcc3b048fc1195b4d629d81117bbb510c
Andro <andrey.v.georgiev@gmail.com>
2018-03-10 21:05:51 +0200

    Added P6 // Prototype Chain EX

---
+++ _02_JavascriptAdvanced/_15_prototypeChainEX/_06_computer/computer.js
@@ -0,0 +1,99 @@
+function createComputerHierarchy() {
+
+    class Keyboard {
+        constructor(manufacturer, responseTime) {
+            this.manufacturer = manufacturer;
+            this.responseTime = responseTime;
+        }
+    }
+
+    class Monitor {
+        constructor(manufacturer, width, height) {
+            this.manufacturer = manufacturer;
+            this.width = Number(width);
+            this.height = Number(height);
+        }
+    }
+
+    class Battery {
+        constructor(manufacturer, expectedLife) {
+            this.manufacturer = manufacturer;
+            this.expectedLife = Number(expectedLife);
+        }
+    }
+
+    class Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace) {
+            if (new.target === Computer) {
+                throw new Error;
+            }
+            this.manufacturer = manufacturer;
+            this.processorSpeed = Number(processorSpeed);
+            this.ram = Number(ram);
+            this.hardDiskSpace = Number(hardDiskSpace);
+        }
+    }
+
+    class Laptop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace, weight, color, battery) {
+            super(manufacturer, processorSpeed, ram, hardDiskSpace);
+            this.weight = weight;
+            this.color = color;
+            this.battery = battery;
+        }
+
+        get battery() {
+            return this._battery;
+        }
+
+        set battery(value) {
+            if (value instanceof Battery) {
+                this._battery = value;
+            } else {
+                throw new TypeError;
+            }
+        }
+    }
+
+    class Desktop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace, keyboard, monitor) {
+            super(manufacturer, processorSpeed, ram, hardDiskSpace);
+            this.keyboard = keyboard;
+            this.monitor = monitor;
+        }
+
+        get keyboard() {
+            return this._keyboard;
+        }
+
+        set keyboard(value) {
+            if (value instanceof Keyboard) {
+                this._keyboard = value;
+                ;
+            } else {
+                throw new TypeError;
+            }
+        }
+
+        get monitor() {
+            return this._monitor;
+        }
+
+        set monitor(value) {
+            if (value instanceof Monitor) {
+                this._monitor = value;
+            } else {
+                throw new TypeError;
+            }
+        }
+    }
+
+    return {
+        Battery,
+        Keyboard,
+        Monitor,
+        Computer,
+        Laptop,
+        Desktop
+    }
+}
